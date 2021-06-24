7b420873d72e954a258d7f30282a22a39bcfe8ea
turutle <turtlehorde@yandex.ru>
2021-06-24 11:58:21 +0300

    Redacted task 3

--- ind3.py
+++ ind3.py
@@ -6,7 +6,8 @@ Created on Fri Jun  4 10:18:58 2021
 """
 
 if __name__ == '__main__':
-    wrd = input("Введите предложение: ")
-
-    ans = wrd.replace(" ", "")
-    print(ans)
+    sent = input("Введите предложение: ")
+    wrd = input("Введите слово: ")
+    ans = sent.count(wrd)
+    for i in range(ans):
+        print(wrd)
